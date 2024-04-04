f763f8cc47e6b77b105e1182894604f8051747ba
IAN ROOT <ianaroot@gmail.com>
2024-04-04 14:37:49 -0700

    correct path in importmap.rb for gameplay js files

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from '/app/javascript/gameplay', under: 'gameplay', to: 'gameplay'
-pin_all_from '/app/assets/images/chess_pieces', under: 'chess_pieces', to: 'chess_pieces'
+pin_all_from 'app/javascript/gameplay', under: 'gameplay', to: 'gameplay'
